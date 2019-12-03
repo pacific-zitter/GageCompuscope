c8bab731de57009eb5efb45a91f82b1844fed96a
unknown <jeffharrison@vivaldi.net>
2019-12-03 09:25:54 -0800

    remove timer outputs.

--- src/threaded.jl
+++ src/threaded.jl
@@ -1,16 +1,15 @@
-using TimerOutputs
-
-
 @kwdef mutable struct Acquire
     gagecard::GageCard
-    isAcquiring = Channel(1)
-    acqReady = Channel(1)
-    transferDone = Channel(1)
-    endAcq::Base.RefValue{Ptr{Cvoid}}
+    isAcquiring::Channel
+    acqReady::Channel
+    transferDone::Channel
+    aborted::Channel
+    endAcq::Base.RefValue{Ptr{Cvoid}} # wrapper around win32 event.
     data::Channel{NTuple{2,Array{Int16,2}}}
 
     Acquire(G::GageCard, nAcq) = new(
     G,
+    Channel(1),
     Channel(1),
     Channel(1),
     Channel(1),
@@ -19,12 +18,8 @@ using TimerOutputs
     )
 end
 
-
-const to = TimerOutput()
-timeit_debug_enabled() = false
-
-function untilReady(G::GageCard;to=1.0,interval=10e-6)
-    cb() = ccall((:CsGetStatus,csssm),Cint,(Cuint,),G.gagehandle) == 0
+function untilReady(G::GageCard; to = 1.0, interval = 10e-6)
+    cb() = ccall((:CsGetStatus, csssm), Cint, (Cuint,), G.gagehandle) == 0
     while true
         cb() && break
         hybrid_sleep(10e-6)
@@ -33,35 +28,29 @@ function untilReady(G::GageCard;to=1.0,interval=10e-6)
 end
 
 function transfer_thread(A::Acquire)
-    CData = MultipleRecord(A.gagecard,1)
-    CData2 = MultipleRecord(A.gagecard,2)
-     while true
-        @timeit_debug to "transfer loop" begin
-            take!(A.acqReady)
-            take!(A.isAcquiring)
-            @timeit_debug to "ch1. transfer" CData()
-            @timeit_debug to "ch2. transfer." CData2()
-            put!(A.transferDone,:ok)
-            put!(A.data, (copy(CData.data_array),copy(CData2.data_array)))
-        end
+    CData = MultipleRecord(A.gagecard, 1)
+    CData2 = MultipleRecord(A.gagecard, 2)
+    while true
+        take!(A.acqReady)
+        take!(A.isAcquiring)
+        CData()
+        CData2()
+        put!(A.transferDone, :ok)
+        put!(A.data, (copy(CData.data_array), copy(CData2.data_array)))
     end
 end
 
-function trigger_thread(A::Acquire;timeout=1.0,interval=10e-3)
+function trigger_thread(A::Acquire; timeout = 1.0, interval = 10e-3)
     acqCount = Ref(0)
+    cb() = get_status(A.gagecard) < 1
     while true
-        cb() = get_status(A.gagecard) < 1
-        @timeit_debug to "trigger loop" begin
-            acqCount[] > A.data.sz_max && break
-            put!(A.isAcquiring,:ok)
-            start(A.gagecard)
-            # @timeit_debug to "wait ready." timedwait(cb,timeout;pollint=interval)
-            @timeit_debug to "wait_custom" untilReady(A.gagecard; interval=1e-6)
-            put!(A.acqReady,:ok)
-            take!(A.transferDone)
-            acqCount[] += 1
-        end
 
+        acqCount[] > A.data.sz_max && break
+        put!(A.isAcquiring, :ok)
+        start(A.gagecard)
+        untilReady(A.gagecard; interval = 1e-6)
+        put!(A.acqReady, :ok)
+        take!(A.transferDone)
+        acqCount[] += 1
     end
-    close.([A.data,A.isAcquiring])
 end
